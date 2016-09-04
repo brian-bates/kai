fb7662445b6a2051226b29dc5204199379497d46
Brian Bates <brian@eccentricprototypes.com>
2016-09-04 15:29:53 -0500

    Update environment.py

--- test/features/environment.py
+++ test/features/environment.py
@@ -3,10 +3,10 @@ import tempfile
 
 
 def before_all(context):
-    # Create a temporary directory to house the test files
+    """Create a temporary directory to house the test files"""
     context.test_dir = tempfile.mkdtemp()
 
 
 def after_all(context):
-    # Remove the directory after the tests are complete
+    """Remove the directory after the tests are complete"""
     shutil.rmtree(context.test_dir)
