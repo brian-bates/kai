1b320d8888dcc2736b028afb6b5f39c703943fae
Brian <Brian.Bates@rackspace.com>
2016-09-29 18:15:49 -0500

    Annother lazy check for python 3 compat

--- kai/kai.py
+++ kai/kai.py
@@ -1,8 +1,9 @@
+from __future__ import absolute_import
 from __future__ import print_function
 
 import argparse
 
-from extractors import ExtractorFactory
+from .extractors import ExtractorFactory
 
 
 def extract(filename, destination='.'):
